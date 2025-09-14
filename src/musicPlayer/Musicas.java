cc3788c6e2d24f329fdc2d6b0dc82ca22facc717
joaomelo13 <161503151+joaomelo13@users.noreply.github.com>
2025-09-14 19:32:40 -0300

    Update Musicas.java

--- src/musicPlayer/Musicas.java
+++ src/musicPlayer/Musicas.java
@@ -6,7 +6,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BuscadordeMusicas {
+public class Musicas {
 
     public static List<String> buscarMusicas() {
         String diretorioUsuario = System.getProperty("user.home");
